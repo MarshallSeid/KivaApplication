7068804f9d207120614f8cdca0ed4d654cc97969
March <Marchness@MM.local>
2015-08-19 11:39:59 -0700

    Outputting kiva api data in calculator

--- routes/calculator.js
+++ routes/calculator.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET Impact Calculator. */
 router.get('/', function(req, res, next) {
-	res.render('calculator', { title: 'Kiva Impact Calculator' });
-	
+	getNewestLoans(20, function(err, response, data){
+		console.log("RESPONNSE: " + response);
+		console.log("DATA: " + data);
+
+		if (response.statusCode != 200) {
+			console.log("ERROR RETURNED");
+			return;
+		}
+		var obj = JSON.parse(data);
+		res.render('calculator', { title: 'Kiva Impact Calculator', body: data});	
+	})
 });
+
+function getNewestLoans(numOfResults, callback) {
+
+	request.get('http://api.kivaws.org/v1/loans/newest.json?per_page=' + numOfResults, callback);
+	 //Callback that uses getlenderinfo
+	//Do more stuff with what I got from the getlenderinfo 
+
+}
+
+
+function getLenderInfo(lenderid, pagenumber, callback) {
+  request.get('http://api.kivaws.org/v1/teams/' + lenderid + '/loans.json?page=' + pagenumber, callback);
+}
 
 /*
 router.get('/lender', function(req, res, next) {
