cf4035dd50a10149e797a56e0d847ec094df85b2
March <Marchness@mm.attlocal.net>
2015-08-26 13:32:25 -0700

    Created Feed.. almost working

--- routes/feed.js
+++ routes/feed.js
@@ -4,7 +4,24 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('feed', { title: 'Kiva Impact Feed' });
-});
+	getNewestLoans(20, function(err, response, data){
+		console.log("RESPONSE: " + response);
+		console.log("DATA: " + data);
+	  if (response.statusCode != 200) {
+	 		console.log("ERROR RETURNED");
+	 		return;
+ 		}
+  res.render('feed', { title: 'Kiva Impact Feed', body: response})
+})});
+
+
+function getNewestLoans(numOfResults, callback) {
+
+	request.get('http://api.kivaws.org/v1/loans/newest.json?per_page=' + numOfResults, callback);
+	//Callback that uses getlenderinfo
+	//Do more stuff with what I got from the getlenderinfo 
+
+}
 
 module.exports = router;
+
