31f958c9056ca3deb3de51dbcce2526186a0645a
March <Marchness@MM.local>
2015-08-31 10:08:57 -0700

    Functionality for the hdi index is nearly complete - trying to understand how callbacks are working

--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+var numberOfResults = 5;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	getNewestLoans(20, function(err, response, data){ 
+	getNewestLoans(numberOfResults, function(err, response, data){ 
 		//console.log("RESPONSE: " + response);
 		//console.log("DATA: " + data);
 	  if (response.statusCode != 200) {
@@ -17,20 +17,33 @@ router.get('/', function(req, res, next) {
   var names = [];
   var countries = [];
   var countryCodes = [];
+  var humanIndex = [];
+
 
   //Other uses we have: Sector (Agriculture, Business), Activity (Tailoring)
   for(i = 0; i < loans.length; i++) {
-  		  names.push(i);
-          names.push(loans[i]["name"]);    
-          countries.push(loans[i]["location"]["country"]);
+  		   names.push(i);
+          names.push(loans[i]["name"]);  
+          countryName = loans[i]["location"]["country"];
+          countries.push(countryName);
           countryCodes.push(loans[i]["location"]["country_code"]);
-
-  }
-  console.log("NAMES: " + names);	
-  console.log("COUNTRIES: " + countries);
+   var returnIndex = getHDIindex(countryName, function(err, response, hdiData){
+            var hdiObj = JSON.parse(hdiData);
+            var hdiRank = hdiObj[0]["hdi_rank"];
+            humanIndex.push(hdiRank);
+            console.log("HDI Ranks in function call: " + humanIndex);
+            return humanIndex;
+          });
+      console.log("RETURN INDEX: " + returnIndex);
+      console.log("HDI Ranks in for loop: " + humanIndex);
+   }
+   console.log("NAMES: " + names);  
+   console.log("COUNTRIES: " + countries);
+   console.log("HDI Ranks: " + humanIndex);
   res.render('feed', { title: 'Kiva Impact Feed', body: names, country: countries,
-  					   cc: countryCodes})
-})});
+               cc: countryCodes, hdi: humanIndex});
+  });
+});
 
 function getNewestLoans(numOfResults, callback) {
 
@@ -38,6 +51,11 @@ function getNewestLoans(numOfResults, callback) {
 	//Callback that uses getlenderinfo
 	//Do more stuff with what I got from the getlenderinfo 
 
+}
+
+function getHDIindex(countryName, callback) {
+  console.log("COUNTRY NAME FROM HDI: " + countryName);
+  request.get('http://data.undp.org/resource/myer-egms.json?country=' + countryName, callback);
 }
 
 
