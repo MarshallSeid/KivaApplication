64357d79d3305e69843271d74dffa227df57f30a
Jason Yang <jasonyang96@ucla.edu>
2016-10-01 09:02:10 -0700

    updated other factors

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ from math import ceil
 
 per_page = 500
 num_pages = 0
-loan_sum = 0
+loan_avg = 0
 
 countries = {}
 
@@ -22,17 +22,43 @@ for i in range(1,num_pages+1):
 	for loan in parsed["loans"]:
 		country_code = str(loan["location"]["country_code"])
 		country_name = str(loan["location"]["country"])
+
 		if country_name in countries:
-			countries[country_name]["loan_sum"] += loan["loan_amount"]
+			countries[country_name]["loan_avg"] += loan["loan_amount"]
+			countries[country_name]["desc_avg"] += len(loan["description"]["languages"])
+			countries[country_name]["tags_avg"] += len(loan["tags"])
+			countries[country_name]["use_avg"] += len(loan["use"])
 			countries[country_name]["num_loans"] += 1
 		else:
 			countries[country_name] = {}
 			countries[country_name]["country_code"] = country_code
-			countries[country_name]["loan_sum"] = loan["loan_amount"]
+			countries[country_name]["desc_avg"] = len(loan["description"]["languages"])
+			countries[country_name]["tags_avg"] = len(loan["tags"])
+			countries[country_name]["use_avg"] = len(loan["use"])
+			countries[country_name]["loan_avg"] = loan["loan_amount"]
 			countries[country_name]["num_loans"] = 1
 
-		#num_desc_lan = len(loan["description"]["languages"])
-
-#print countries
+# for loop to avg all sums
 for country in countries:
-	print (country + ": " + str(countries[country]["loan_sum"] / countries[country]["num_loans"]))
+	num_loans = countries[country]["num_loans"]
+	countries[country]["loan_avg"] /= float(num_loans)
+	countries[country]["desc_avg"] /= float(num_loans)
+	countries[country]["tags_avg"] /= float(num_loans)
+	countries[country]["use_avg"] /= float(num_loans)
+
+
+#print countries' loan avg
+for country in countries:
+	print ("loan average length for " + country + ": " + str(countries[country]["loan_avg"]))
+
+#print countries' desc avg
+for country in countries:
+	print ("desc average length for " + country + ": " + str(countries[country]["desc_avg"]))
+
+#print countries' use avg
+for country in countries:
+	print ("use average length for " + country + ": " + str(countries[country]["use_avg"]))
+
+#print countries' tag avg
+for country in countries:
+	print ("tag average length for " + country + ": " + str(countries[country]["tags_avg"]))
