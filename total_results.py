e126bd6a522b82c1ad1f2c6072d5f67d9b9cd0a5
Marshall Seid <Marchness@MM-2.local>
2016-10-02 00:04:05 -0700

    Script working, going to change it to only work on funded

--- total_results.py
+++ total_results.py
@@ -10,80 +10,267 @@ loan_avg = 0
 
 data = { 
 	"funded": {
+		"total_funds": 0.0,
 		"countries": {},
+		"activity": {},
 		"tag": {},
-		"gender": {
-			"male": 0.0,
-			"female": 0.0
-		},
+		"sector": {},
+		"themes": {},
+		"num_loans": 0.0
+	},
+	"fundraising": {
+		"total_funds": 0.0,
+		"countries": {},
+		"activity": {},
+		"tag": {},
 		"sector": {},
 		"themes": {},
 		"num_loans": 0.0
 	},
 	"expired": {
+		"total_funds": 0.0,
 		"countries": {},
+		"activity": {},
 		"tag": {},
-		"gender": {
-			"male": 0.0,
-			"female": 0.0
-		},
 		"sector": {},
 		"themes": {},
 		"num_loans": 0.0
 	},
 	"total": {
+		"total_funds": 0.0,
 		"countries": {},
+		"activity": {},
 		"tag_funded": {},
-		"gender_funded": {
-			"male": 0.0,
-			"female": 0.0
-		},
 		"sector": {},
 		"themes": {},
 		"num_loans": 0.0
 	}
 }
 
-req = get("http://api.kivaws.org/v1/loans/search.json?per_page=1").content
+averages = {
+	"sectors": {
+		 "Agriculture": {
+		 	"avg_funded": 0.0,
+			 "num_loans": 0.0,
+			 "total_funds": 0.0},
+		 "Arts":{
+			 "avg_funded": 0.0,
+			 "num_loans": 0.0,
+			 "total_funds": 0.0}, 
+		 "Clothing":{
+			 "avg_funded": 0.0,
+			 "num_loans": 0.0,
+			 "total_funds": 0.0}, 
+		 "Construction":{
+		 	"avg_funded": 0.0,
+			 "num_loans": 0.0,
+			 "total_funds": 0.0}, 
+		 "Education":{
+		 	"avg_funded": 0.0,
+			 "num_loans": 0.0,
+			 "total_funds": 0.0}, 
+		 "Entertainment":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Food":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Health":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Housing":{
+			"avg_funded": 0.0,	
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Manufacturing":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Personal Use":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Retail":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Services":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Transportation":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}, 
+		 "Wholesale":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}
+	},
+	"themes": {		
+		"Green":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Higher Education":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Arab Youth":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Kiva City LA":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Islamic Finance":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Youth":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Start-Up":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Water and Sanitation":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Vulnerable Groups":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Fair Trade":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Rural Exclusion":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Mobile Technology":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Underfunded Areas":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Conflict Zones":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Job Creation":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "SME":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Growing Businesses":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Kiva City Detroit":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Health":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Disaster recovery":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Flexible Credit Study":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Innovative Loans":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "IPA Study":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0},
+		 "Refugees/Displaced":{
+		 	"avg_funded": 0.0,
+			"num_loans": 0.0,
+			"total_funds": 0.0}
+		} 
+	}
+
+req = get("http://api.kivaws.org/v1/loans/search.json?status=funded&per_page=1").content
 parsed = loads(req)
 num_loans = parsed["paging"]["total"]
 num_pages = int(ceil(num_loans / per_page))
 
 # for loop for each page
-for i in range(1, 20):
-	req = get("http://api.kivaws.org/v1/loans/search.json?per_page="+str(per_page)+"&page=" + str(i)).content
+for i in range(1, 10):
+	req = get("http://api.kivaws.org/v1/loans/search.json?status=funded&per_page="+str(per_page)+"&page=" + str(i)).content
 	parsed = loads(req)
 	# for loop for each loan
 	for loan in parsed["loans"]:
-		# Increment Countries
-		if data[loan["status"]]["countries"][loan[country_code]] == 0:
-			data[loan["status"]]["countries"][loan[country_code]] = 1;
-		else: 
-			data[loan["status"]]["countries"][loan[country_code]] += 1;
-		# Increment Gender	
-		data[loan["status"]]["gender_funded"][loan[gender]] += 1;
-		data["total"]["gender_funded"][loan[gender]] += 1;
-		# Increment tag funded
-		if data[loan["status"]]["sector"][loan["sector"]] == 0:
-			data[loan["status"]]["sector"][loan["sector"]] = 1;
-		else: 
-			data[loan["status"]]["sector"][loan["sector"]] += 1;
+		status = loan["status"]
+		if status == "funded":
+			# Increment Countries
+			country  = loan["location"]["country"]
+			loan_amt = loan["funded_amount"]
+			if country not in data[loan["status"]]["countries"]:
+				data[status]["countries"][country] = 1
+			else: 
+				data[status]["countries"][country] += 1
 
-		data[loan["status"]]["num_loans"] += 1
+			# Increment sector funded
+			sector = loan["sector"]
+			if sector not in data[status]["sector"]:
+				data[status]["sector"][sector] = 1
+				averages["sectors"][sector]["num_loans"] = 1
+			else: 
+				data[status]["sector"][sector] += 1
+				averages["sectors"][sector]["num_loans"] += 1
 
-		# for tag in loan["tags"]:
-		# 	tag_name = tag["name"] 
-		# 	if tag_name in loans[loan["status"]]["tag"]:
-		# 		loans[loan["status"]]["tag"][tag_name] += 1
-		# 	else:
-		# 		loans[loan["status"]]["tag"][tag_name] = 1
-		# 	if tag_name in totals:
-		# 		totals[tag_name] += 1
-		# 	else: 
-		# 		totals[tag_name] = 1.0
+			# Increment theme funded
+			if "themes" in loan:
+				for theme in loan["themes"]:
+					if theme not in data[loan["status"]]["themes"]:
+						data[status]["themes"][theme] = 1
+						averages["themes"][theme]["num_loans"] = 1
+					else: 
+						data[status]["themes"][theme] += 1
+						averages["themes"][theme]["num_loans"] += 1
 
+			# Increment activity funded
+			if loan["activity"] not in data[status]["activity"]:
+				data[status]["activity"][loan["activity"]] = 1
+			else: 
+				data[status]["activity"][loan["activity"]] += 1
+				averages["sectors"][sector]["total_funds"] += loan_amt
+				averages["themes"][theme]["total_funds"] += loan_amt
+			data[status]["num_loans"] += 1
+			data["total"]["num_loans"] += 1
+		
+#for activity in data["funded"]["activity"]:
+for sector in averages["sectors"]:
+	sector["avg_funded"] = sector["total_funds"] / sector["num_loans"]
+for theme in averages["themes"]:
+	theme["avg_funded"] = theme["total_funds"] / theme["num_loans"]
+
+# Update the Funded Campaigns/Total Campaigns
 with open('data.txt', 'w') as outfile:
 	json.dump(data, outfile)
+with open('averages.txt', 'w') as outfile:
+	json.dump(averages, outfile)
 
 # for name in data["funded"]["tag"]:
 # 	if data["funded"]["tag"][name] > 10:
